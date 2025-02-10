fb1e751ce97720721234c346f99a3f646cf6c597
yousef <tyousef262@gmail.com>
2025-02-10 21:26:43 +0200

    c1

--- feat4.js
+++ feat4.js
@@ -1,4 +1,7 @@
-function divide() {
-    return 10 * 2;
-  }
-  
+const function12 = (sum1, sum2) => {
+  const num1 = sum1;
+  const num2 = sum2;
+
+  const result = num1 + num2;
+  return result;
+};
